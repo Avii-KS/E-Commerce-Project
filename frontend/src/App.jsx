78b44d0d99f0c7f30d38aa160ac2bd897a3c0685
Avii-KS <avi.k.s.4653@gmail.com>
2025-01-18 15:17:53 +0530

    Changes Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { LoginPage } from "./routes/route.js";
+import SignupPage from "./pages/signupPage.jsx";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   );
